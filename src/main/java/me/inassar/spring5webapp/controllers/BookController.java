f1a014a4854a64c474d9d9a0af5dcf1ddce271c0
ranger163 <ahmed.nassar163@gmail.com>
2020-03-28 15:54:34 +0200

    Working with controllers Adding BookController.java

---
+++ src/main/java/me/inassar/spring5webapp/controllers/BookController.java
@@ -0,0 +1,22 @@
+package me.inassar.spring5webapp.controllers;
+
+import me.inassar.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private BookRepository repository;
+
+    public BookController(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("books",repository.findAll());
+        return "books";
+    }
+}
